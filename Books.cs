19e2aa719197df7deec8f666f92cdb1e75fb7599
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-15 23:22:20 -0800

    Add project files.

---
+++ Books.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Constructors_JuanHernandez
+{
+    internal class Books
+    {
+        private string title;
+        private string author;
+
+        
+        public Books() // Default constructor: will have title and author filled
+        {
+            title = "n/a";
+            author = "n/a";
+        }// user has no information
+        public Books(string title) //constructor 3: will require title from user, and no author
+        {
+            this.title = title;
+            author = "n/a";
+        }// incase user only has title
+        public Books(string title, string author) //constructor 2: will require user to enter title and author
+        {
+            this.title = title;
+            this.author = author;
+        }// if user has both author and title
+        //contructor overloading provides flexibility b/c it allows user not to have all the information ready but make modifications later on
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GA_Constructors_JuanHernandez
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exploring Constructors in C#");
+
+            Books book1 = new Books("1982", "The Color Purple");
+            Console.WriteLine($"Book 1: Title = {book1.Title}, Author = {book1.Author}");
+
+            Books book2 = new Books("Cat in the Hat");
+            Console.WriteLine($"Book 2: Title = {book2.Title}, Author = {book2.Author}");
+        }
+    }
+}
